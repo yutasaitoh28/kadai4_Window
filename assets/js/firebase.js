8f3cd88df8de7e1bc53e2ae55a03baa15a6ff062
yutasaitoh28 <33260997+yutasaitoh28@users.noreply.github.com>
2022-04-27 07:10:40 +0900

    add firebase

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,23 +1,11 @@
 const firebaseConfig = {
-
-    apiKey: "AIzaSyCcMrqMswXRwqVWz2hXF65c04byzQvT9X8",
-  
-    authDomain: "fir-chat-7431a.firebaseapp.com",
-  
-    databaseURL: "https://fir-chat-7431a-default-rtdb.firebaseio.com",
-  
-    projectId: "fir-chat-7431a",
-  
-    storageBucket: "fir-chat-7431a.appspot.com",
-  
-    messagingSenderId: "254460273386",
-  
-    appId: "1:254460273386:web:f72a218fa9b037f8642d93"
-  
-  };
+  apiKey: "AIzaSyC_YbS159teIeHG2FuqqUb5GKbfonwp-LQ",
+  authDomain: "fir-chat-81cb0.firebaseapp.com",
+  databaseURL: "https://fir-chat-81cb0-default-rtdb.firebaseio.com",
+  projectId: "fir-chat-81cb0",
+  storageBucket: "fir-chat-81cb0.appspot.com",
+  messagingSenderId: "723428276900",
+  appId: "1:723428276900:web:f672f9c296144439964431"
+};
 
 firebase.initializeApp(firebaseConfig);
-
-const db = firebase.database().ref();
-const auth = firebase.auth();
-auth.useDeviceLanguage();
